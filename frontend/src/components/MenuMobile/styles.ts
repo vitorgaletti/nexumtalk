90b1728c3af8f82e1d8838b195e8bf16f8e182b4
Vitor <vitor.fgaletti@gmail.com>
2022-11-25 22:04:52 -0300

    💄 Update height MenuMobile in Home

--- frontend/src/components/MenuMobile/styles.ts
+++ frontend/src/components/MenuMobile/styles.ts
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 export const MenuMobileContainer = styled.div`
   max-width: calc(100vw - 30%);
   width: 100%;
-  height: 60vh;
+  height: 55vh;
   padding: 1rem;
   background-color: white;
   position: fixed;
